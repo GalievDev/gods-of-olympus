650307875b2e5558afd3f784d578285f0efcc894
GalievDev <ggvalion@gmail.com>
2023-12-18 15:54:21 +0300

    God's punishment

--- src/main/java/dev/galiev/gofo/events/ServerLifeCycle.java
+++ src/main/java/dev/galiev/gofo/events/ServerLifeCycle.java
@@ -2,13 +2,30 @@ package dev.galiev.gofo.events;
 
 import dev.galiev.gofo.utils.GodsLists;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 
-public class ServerLifeCycle implements ServerTickEvents.StartWorldTick{
+public class ServerLifeCycle implements ServerTickEvents.StartWorldTick {
+    private static final int delay = 400;
+    private static int counter = 0;
+    private static boolean punished = false;
+
     @Override
     public void onStartTick(ServerWorld world) {
-        if (!GodsLists.getNeptuneBlackList().isEmpty()) {
+        int pDelay = 300;
+        if (counter == pDelay) punished = false;
+        if (counter < 0) counter = 0;
 
-        }
+        if (counter == delay) {
+            if (!GodsLists.getNeptuneBlackList().isEmpty() && !punished) {
+                for (PlayerEntity player : GodsLists.getNeptuneBlackList()) {
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 400));
+                    punished = true;
+                }
+            }
+            counter = 0;
+        } else counter++;
     }
 }
