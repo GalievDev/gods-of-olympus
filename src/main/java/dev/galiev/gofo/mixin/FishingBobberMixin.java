a83196ffbf1dd3f2fdb08cd8b693d42900b74f3e
GalievDev <ggvalion@gmail.com>
2023-12-23 18:39:43 +0300

    Punishment of fishing

--- src/main/java/dev/galiev/gofo/mixin/FishingBobberMixin.java
+++ src/main/java/dev/galiev/gofo/mixin/FishingBobberMixin.java
@@ -2,7 +2,7 @@ package dev.galiev.gofo.mixin;
 
 import dev.galiev.gofo.utils.GodsData;
 import dev.galiev.gofo.utils.IPlayerDataSaver;
-import net.minecraft.entity.ItemEntity;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.ItemStack;
@@ -11,24 +11,23 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
 
 @Mixin(FishingBobberEntity.class)
 public abstract class FishingBobberMixin {
-
     @Shadow @Nullable public abstract PlayerEntity getPlayerOwner();
 
-    @Shadow private int hookCountdown;
-
-    @Inject(method = "use", at = @At(value = "TAIL"))
-    private void canFish(ItemStack usedItem, CallbackInfoReturnable<Integer> cir) {
+    @ModifyVariable(method = "use", at = @At(value = "STORE"), ordinal = 0)
+    private List<ItemStack> changeCatch(List<ItemStack> list) {
         var player = this.getPlayerOwner();
         if (GodsData.isNeptuneHate((IPlayerDataSaver) player)) {
-            if (this.hookCountdown > 0) {
-                ItemEntity entity = new ItemEntity(player.getWorld(), player.getX(), player.getY(), player.getZ(), new ItemStack(Items.PUFFERFISH));
-                player.getWorld().spawnEntity(entity);
-            }
+            ObjectArrayList<ItemStack> newList = new ObjectArrayList<>();
+            newList.add(new ItemStack(Items.PUFFERFISH));
+            return newList;
+        } else {
+        return list;
         }
     }
 }
