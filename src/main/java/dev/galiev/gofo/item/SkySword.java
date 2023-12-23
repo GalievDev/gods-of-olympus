ad2b19f158e9dd8efcd0b73dbf37d55781e9ce91
GalievDev <ggvalion@gmail.com>
2023-12-23 19:40:20 +0300

    Duration changed

--- src/main/java/dev/galiev/gofo/item/SkySword.java
+++ src/main/java/dev/galiev/gofo/item/SkySword.java
@@ -18,7 +18,7 @@ public class SkySword extends SwordItem {
             target.takeKnockback(1.0, attacker.getX() - target.getX(), attacker.getZ() - target.getZ());
 
             target.teleport(target.getX(), target.getY() + 5, target.getZ());
-            target.addStatusEffect(new StatusEffectInstance(EffectsRegistry.WRATH_OF_HEAVEN, 150, 1));
+            target.addStatusEffect(new StatusEffectInstance(EffectsRegistry.WRATH_OF_HEAVEN, 75, 1));
         }
         return super.postHit(stack, target, attacker);
     }
