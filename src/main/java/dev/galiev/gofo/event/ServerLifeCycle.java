2efa3245dd9861287914bfc28a0eb355b99fdf82
GalievDev <ggvalion@gmail.com>
2023-12-23 18:39:26 +0300

    Random ticks punishes

--- src/main/java/dev/galiev/gofo/event/ServerLifeCycle.java
+++ src/main/java/dev/galiev/gofo/event/ServerLifeCycle.java
@@ -14,15 +14,16 @@ import net.minecraft.world.World;
 import static dev.galiev.gofo.GodsOfOlympus.RANDOM;
 
 public class ServerLifeCycle implements ServerTickEvents.StartWorldTick {
-    private static final int delay = 400;
+    private static final Random random = Random.create();
+    private static final int delay = RANDOM.nextInt(7000, 15000);
     private static int counter = 0;
     private static boolean nPunished = false;
     private static boolean jPunished = false;
 
     @Override
     public void onStartTick(ServerWorld world) {
-        int nDelay = 300;
-        int jDelay = 200;
+        int nDelay = delay - 1000;
+        int jDelay = delay - 2000;
         if (counter == nDelay) nPunished = false;
         if (counter == jDelay) jPunished = false;
         if (counter < 0) counter = 0;
@@ -45,7 +46,6 @@ public class ServerLifeCycle implements ServerTickEvents.StartWorldTick {
 
     private void spawnGuardians(World world, PlayerEntity player) {
         int rand = RANDOM.nextInt(1, 10);
-        Random random = Random.create();
         for (BlockPos pos : BlockPos.iterateRandomly(random, rand, player.getBlockPos(), 5)) {
             EntityType.GUARDIAN.spawn((ServerWorld) world, pos, SpawnReason.EVENT);
         }
@@ -53,7 +53,6 @@ public class ServerLifeCycle implements ServerTickEvents.StartWorldTick {
 
     private void createLighthing(PlayerEntity player) {
         int rand = RANDOM.nextInt(2, 5);
-        Random random = Random.create();
         EntityType.LIGHTNING_BOLT.spawn((ServerWorld) player.getWorld(), player.getBlockPos(), SpawnReason.EVENT);
         for (BlockPos pos : BlockPos.iterateRandomly(random, rand, player.getBlockPos(), 5)) {
             EntityType.LIGHTNING_BOLT.spawn((ServerWorld) player.getWorld(), pos, SpawnReason.EVENT);
