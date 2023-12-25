aed247fd000699182e0d17647922e45604a3db3d
GalievDev <ggvalion@gmail.com>
2023-12-25 14:37:40 +0300

    Player First Join

---
+++ src/main/java/dev/galiev/gofo/mixin/PlayerManagerMixin.java
@@ -0,0 +1,23 @@
+package dev.galiev.gofo.mixin;
+
+import dev.galiev.gofo.utils.GodsData;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerManager.class)
+public class PlayerManagerMixin {
+    @Inject(at = @At(value = "TAIL"), method = "onPlayerConnect")
+    private void onPlayerJoin(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo ci) {
+        if (player.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.LEAVE_GAME)) < 1) {
+            GodsData.addRepJupiter(player, (short) 7);
+            GodsData.addRepNeptune(player, (short) 7);
+        }
+    }
+}
