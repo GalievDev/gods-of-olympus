9eb86e0cc2b62a2b4ed5335615fe277993ea86ff
GalievDev <ggvalion@gmail.com>
2023-12-27 15:41:34 +0300

    Creating Zeus Lightning

--- src/main/java/dev/galiev/gofo/block/ZeusStatue.java
+++ src/main/java/dev/galiev/gofo/block/ZeusStatue.java
@@ -1,19 +1,30 @@
 package dev.galiev.gofo.block;
 
+import dev.galiev.gofo.registry.ItemsRegistry;
+import dev.galiev.gofo.utils.GodsData;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class ZeusStatue extends Block {
@@ -24,6 +35,24 @@ public class ZeusStatue extends Block {
     }
 
     private static final VoxelShape SHAPE = Block.createCuboidShape(0, -16, 0, 16, 32, 16);
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (world.isClient) return ActionResult.FAIL;
+        if (GodsData.getRepZeus(player) == 15) {
+            var stack = player.getStackInHand(hand);
+            if (stack.getItem() == Items.NETHERITE_SWORD) {
+                EntityType.LIGHTNING_BOLT.spawn((ServerWorld) world, pos, SpawnReason.EVENT);
+                var blocPos = new BlockPos.Mutable(pos.getX(), pos.getY() + 3, pos.getZ());
+                world.breakBlock(blocPos, false);
+                blocPos.setY(pos.getY() + 4);
+                world.breakBlock(blocPos, false);
+                world.createExplosion(null, blocPos.getX(), blocPos.getY(), blocPos.getZ(), 2.0F, World.ExplosionSourceType.BLOCK);
+                player.setStackInHand(hand, ItemsRegistry.ZEUS_LIGHTNING.getDefaultStack());
+            }
+        }
+        return super.onUse(state, world, pos, player, hand, hit);
+    }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
