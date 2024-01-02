3541142fd38953e230ac2b7c993a32fbcaf52217
GalievDev <ggvalion@gmail.com>
2024-01-02 11:51:12 +0300

    Bug fixed

--- src/main/java/dev/galiev/gofo/block/ZeusStatue.java
+++ src/main/java/dev/galiev/gofo/block/ZeusStatue.java
@@ -62,7 +62,7 @@ public class ZeusStatue extends Block {
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
 
     @Override
